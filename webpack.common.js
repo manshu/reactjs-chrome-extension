b0f1bc49d52271ca555d00936b3f3d3ccf3c9e6c
Himanshu Batra <himanshu@batra.io>
2022-05-21 01:06:53 -0400

    WiP

--- webpack.common.js
+++ webpack.common.js
@@ -1,10 +1,9 @@
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const HtmlPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'cheap-module-source-map',
     entry: {
         popup: path.resolve('src/popup/popup.tsx'),
         options: path.resolve('src/options/options.tsx'),
@@ -29,6 +28,9 @@ module.exports = {
         ]
     },
     "plugins": [
+        new CleanWebpackPlugin({
+            cleanStaleWebpackAssets: false
+        }),
         new CopyPlugin({
             patterns: [{
                 from: path.resolve('src/static'),
